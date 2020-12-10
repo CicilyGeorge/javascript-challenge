04a2f63eaa339f2bb6befd345d128ffde420df16
CicilyGeorge <69083251+CicilyGeorge@users.noreply.github.com>
2020-12-11 04:27:10 +0800

    UFO Level2 coding

--- Code/UFO-level-2/static/js/app.js
+++ Code/UFO-level-2/static/js/app.js
@@ -23,17 +23,116 @@ function displayTable(rows){
     }
 }
 
+function loadDropdowns(){
+    // Getting an array of dates in data
+    let dateArray = tableData.map(sighting => sighting.datetime);
+    dateArray.push("");
+    // Converting the array into a set to keep unique city names
+    let dateList = d3.set(dateArray).values();
+    // Selecting the seclect element and appending option tags
+    const selectDate = d3.select("#datetime");
+    var options = selectDate.selectAll('option')
+                            .data(dateList)
+                            .enter()
+                            .append('option')
+                            .attr("value",(d) => d)
+                            .text((d) => d)
+                            .property("selected", (d) => d == "" );
+
+
+    // Getting an array of city names in data
+    let cityArray = tableData.map(sighting => sighting.city);
+    cityArray.push("");
+    // Converting the array into a set to keep unique city names
+    let cityList = d3.set(cityArray).values();
+    cityList = cityList.sort(d3.ascending);
+    // Selecting the seclect element and appending option tags
+    const selectCity = d3.select("#city");
+    var options = selectCity.selectAll('option')
+                            .data(cityList)
+                            .enter()
+                            .append('option')
+                            .attr("value",(c) => c)
+                            .text((c) => c)
+                            .property("selected", (d) => d == "" );
+
+
+    // Getting an array of state names in data
+    let stateArray = tableData.map(sighting => sighting.state);
+    stateArray.push("");
+    // Converting the array into a set to keep unique state names
+    let stateList = d3.set(stateArray).values();
+    stateList = stateList.sort(d3.ascending);
+    // Selecting the seclect element and appending option tags
+    const selectState = d3.select("#state");
+    var options = selectState.selectAll('option')
+                            .data(stateList)
+                            .enter()
+                            .append('option')
+                            .attr("value",(s) => s)
+                            .text((s) => s)
+                            .property("selected", (d) => d == "" );    
+                            
+    // Getting an array of country names in data
+    let countryArray = tableData.map(sighting => sighting.country);
+    countryArray.push("");
+    // Converting the array into a set to keep unique country names
+    let countryList = d3.set(countryArray).values();
+    countryList = countryList.sort(d3.ascending);
+    // Selecting the seclect element and appending option tags
+    const selectCountry = d3.select("#country");
+    var options = selectCountry.selectAll('option')
+                            .data(countryList)
+                            .enter()
+                            .append('option')
+                            .attr("value",(c) => c)
+                            .text((c) => c)
+                            .property("selected", (d) => d == "" );    
+                            
+    // Getting an array of shape names in data
+    let shapeArray = tableData.map(sighting => sighting.shape);
+    shapeArray.push("");
+    // Converting the array into a set to keep unique shape names
+    let shapeList = d3.set(shapeArray).values();
+    shapeList = shapeList.sort(d3.ascending);
+    // Selecting the seclect element and appending option tags
+    const selectShape = d3.select("#shape");
+    var options = selectShape.selectAll('option')
+                            .data(shapeList)
+                            .enter()
+                            .append('option')
+                            .attr("value",(s) => s)
+                            .text((s) => s)
+                            .property("selected", (d) => d == "" );   
+}
+
 
 // Action to perform on events
 function Action(){
     // Prevent the page from refreshing
     d3.event.preventDefault();
-    // Select the date input element in the form
-    const inputDtElement = d3.select("#datetime");
-    // Get the value of the input element
-    let inputDate = inputDtElement.property("value");
+
+    // Get the value of the input elements in the form
+    let datetime = d3.select("#datetime").node().value;
+    let city = d3.select("#city").node().value;
+    let state= d3.select("#state").node().value;
+    let country = d3.select("#country").node().value;
+    let shape = d3.select("#shape").node().value;
+
+
+    // Creating an array of data set keys to apply filter
+    let filters = [];
+    if(datetime !== "") filters.push("datetime");
+    if(city !== "") filters.push("city");
+    if(state !== "") filters.push("state");
+    if(country !== "") filters.push("country");
+    if(shape !== "") filters.push("shape");
+
+    let filteredData = [];
+    forEach(key in filters) 
+        filteredData = filteredData.push(tableData.filter(sightings => sightings.key == key));
     
-    let filteredData = tableData.filter(sightings => sightings.datetime == inputDate);
+
 
     // Clearing table before appending filterd data rows
     tbody.remove();
@@ -49,10 +148,13 @@ function Action(){
 
 // Display full table on page load
 displayTable(tableData);
+// Calling loadDropdowns to fill dropdown list on page load
+loadDropdowns();
 
-// Select the submit button for filter
-const submit = d3.select("#filter-btn");
+// Select the form and form elements for filter
 const form = d3.select("#searchForm");
+const submit = d3.select("#filter-btn");
+
 
 // Calling the Action function on click and form submit events
 submit.on("click", Action);
